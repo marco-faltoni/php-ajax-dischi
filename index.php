fdcd2b83fcffed9b9eae5f9775c52f7a38c024c6
Marco <marco.faltoni@outlook.it>
2020-06-17 15:16:45 +0200

    Inizio

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Dischi Musicali</title>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js"></script>
+        <link rel="stylesheet" href="public/app.css">
+    </head>
+
+    <body>
+
+
+
+        <script src="public/app.js"></script>
+    </body>
+</html>
